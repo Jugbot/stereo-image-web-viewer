3c7f2394b2b5cd1b7a10984e08c4a909b28063c5
Lucas Pollice <lp1935@nyu.edu>
2024-03-06 12:31:46 -0500

    qs

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,142 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { ScreenSpace } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Controllers, Hands, VRButton, XR } from "@react-three/xr";
+import { ChangeEventHandler, useState } from "react";
 import "./App.css";
+import { VRImages } from "./Images";
+
+// function Eye() {
+//   const { referenceSpace} = useXR();
+//   const pose = frame.getViewerPose(referenceSpace);
+//   for (const view of pose.views) {
+//     const viewport = glLayer.getViewport(view);
+//     gl.viewport(viewport.x, viewport.y, viewport.width, viewport.height);
+
+//     if (view.eye === 'left') {
+//       // Render the image to the left eye
+//       renderImageToEye(view, 'left');
+//     } else if (view.eye === 'right') {
+//       // Render the image to the right eye
+//       renderImageToEye(view, 'right');
+//     }
+//   }
+// }
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [leftEyeImage, setLeftEyeImage] = useState<string | null>(null);
+  const [rightEyeImage, setRightEyeImage] = useState<string | null>(null);
+
+  const handleLeftEyeImageChange: ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    if (!event.target.files) {
+      setLeftEyeImage(null);
+      return;
+    }
+    setLeftEyeImage(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const handleRightEyeImageChange: ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    if (!event.target.files) {
+      setRightEyeImage(null);
+      return;
+    }
+    setRightEyeImage(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const handleSwapImages = () => {
+    const temp = leftEyeImage;
+    setLeftEyeImage(rightEyeImage);
+    setRightEyeImage(temp);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'start', gap: '8px', padding: '8px'}}>
+        <label htmlFor="left-eye">Upload Left Eye Image</label>
+        <input
+          type="file"
+          id="left-eye"
+          name="left-eye"
+          accept="image/*"
+          onChange={handleLeftEyeImageChange}
+        />
+        <label htmlFor="right-eye">Upload Right Eye Image</label>
+        <input
+          type="file"
+          id="right-eye"
+          name="right-eye"
+          accept="image/*"
+          onChange={handleRightEyeImageChange}
+        />
+        <button onClick={handleSwapImages}>Swap images</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <VRButton />
+      <Canvas>
+        <XR>
+          <Controllers />
+          <Hands />
+
+          {/* <mesh>
+            <planeGeometry />
+            <boxGeometry />
+            <meshBasicMaterial color="blue" />
+          </mesh> */}
+          <ScreenSpace
+            depth={10} // Distance from camera
+          >
+            <VRImages
+              leftEyeImage={leftEyeImage}
+              rightEyeImage={rightEyeImage}
+            />
+          </ScreenSpace>
+          {/* <mesh
+            onBeforeRender={(
+              renderer,
+              scene,
+              camera,
+            ) => {
+              const frame = renderer.xr.getFrame();
+              const referenceSpace = renderer.xr.getReferenceSpace();
+              if (!referenceSpace) {
+                return;
+              }
+              const pose = frame.getViewerPose(referenceSpace);
+              const session = frame.session;
+              if (!pose) {
+                return;
+              }
+              const leftEye = session.renderState.baseLayer?.getViewport(pose.views[0]);
+              const rightEye = session.renderState.baseLayer?.getViewport(pose.views[1]);
+              // console.log('Left eye camera viewport:', leftEye);
+              // console.log('Right eye camera viewport:', rightEye);
+              if (texture && leftEye && rightEye) {
+                renderer.setRenderTarget(null);
+                renderer.clear();
+                renderer.setScissorTest(true);
+
+                // Render to the left eye
+                renderer.setViewport(leftEye.x, leftEye.y, leftEye.width, leftEye.height);
+                renderer.setScissor(leftEye.x, leftEye.y, leftEye.width, leftEye.height);
+                // renderer.render(scene, camera);
+
+                // Render to the right eye
+                // renderer.setViewport(rightEye.x, rightEye.y, rightEye.width, rightEye.height);
+                // renderer.setScissor(rightEye.x, rightEye.y, rightEye.width, rightEye.height);
+                // renderer.render(scene, camera);
+
+                // renderer.setScissorTest(false);
+              }
+            }}
+          >
+            <planeGeometry />
+            <meshBasicMaterial color="red" />
+            <meshBasicMaterial map={texture}/>
+          </mesh> */}
+        </XR>
+      </Canvas>
     </>
   );
 }
